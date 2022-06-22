7dd8c008c5340c31cc7db8b6a945aa8d69010ab6
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-06-22 14:32:26 +0300

    L007 - Added duck & bird

---
+++ src/com/company/animals/Bird.java
@@ -0,0 +1,17 @@
+package com.company.animals;
+
+public class Bird extends Animal{
+    protected Boolean flyModeActive = false;
+
+    public void takeOff(){
+        flyModeActive = true;
+    }
+
+    public void landing(){
+        flyModeActive = false;
+    }
+
+    public void isTheBirdFlying(){
+        System.out.println(flyModeActive ? "Duck mode fly - yes" : "Duck mode fly - no");
+    }
+}
