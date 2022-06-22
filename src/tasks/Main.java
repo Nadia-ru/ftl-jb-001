8fa7315268e32a6b154a8f38bb8f8f5b038c8c46
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-06-22 15:21:12 +0300

    task2

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -2,9 +2,11 @@ package tasks;
 
 
 import tasks.task001.Task001;
+import tasks.task002.Task002;
 
 public class Main {
     public static void main (String[] args) {
-        Task001.run();
+//        Task001.run();
+        Task002.run();
     }
 }
---
+++ src/tasks/task002/Task002.java
@@ -0,0 +1,11 @@
+package tasks.task002;
+
+
+public class Task002 {
+
+    public static void run() {
+        for (int i = 5; i >= 1; i--){
+            System.out.printf("%d ", i);
+        }
+    }
+}
