864f43e5d1ea6969ac8d537800eea79e2dba6e26
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-06-23 20:47:27 +0300

    task005

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -1,19 +1,12 @@
 package tasks;
 
 
-import tasks.task001.Task001;
-import tasks.task002.Task002;
-import tasks.task003.Task003;
-import tasks.task004.Task004;
+import tasks.task005.Task005;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input a number: ");
-        int num = in.nextInt();
-        Task004.run(num);
-        in.close();
+        Task005.run();
     }
 }
