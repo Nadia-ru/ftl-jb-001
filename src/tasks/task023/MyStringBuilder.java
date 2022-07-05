63de0dd333b8eda118d70c86b954899b98ab4413
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-07-05 06:10:54 +0300

    task023

---
+++ src/tasks/task023/MyStringBuilder.java
@@ -0,0 +1,59 @@
+package tasks.task023;
+
+public class MyStringBuilder {
+
+
+    private StringBuilder controls = new StringBuilder();
+
+    public MyStringBuilder(StringBuilder controls) {
+        this.controls = controls;
+    }
+
+    public char charAt(int x){
+        return this.controls.charAt(x);
+    }
+    public void setCharAt(int x, char a){
+        this.controls.setCharAt(x, a);
+    }
+
+    public void reverse(){
+        this.controls.reverse();
+    }
+
+    public String toString(){
+
+        return this.controls.toString();
+    }
+
+    public StringBuilder replace(int x1,int x2, String z){
+        return this.controls.replace(x1,x2,z);
+    }
+
+    public StringBuilder deleteCharAt(int x){
+        return  this.controls.deleteCharAt(x);
+    }
+
+    public StringBuilder append(String x){
+        return this.controls.append(x);
+    }
+
+    public int length()
+    {
+        return this.controls.length();
+    }
+
+    public static void undo(){
+        StringBuilder x =new StringBuilder("nasdf");
+        MyStringBuilder strB =new MyStringBuilder(x);
+
+        int y =0;
+        int y1 = 3;
+        char y2 ='a';
+        System.out.println(strB.charAt(y));
+        strB.setCharAt(y,y2);
+        System.out.println(strB);
+        strB.deleteCharAt(y1);
+        System.out.println(strB);
+
+    }
+}
