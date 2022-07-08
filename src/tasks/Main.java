1e48465cf04aaf44484ba3c8cbd9590eaa68d766
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-07-08 22:15:42 +0300

    task26

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -1,31 +1,27 @@
 package tasks;
 
 import tasks.task025.Filter;
-import tasks.task025.Task25;
+import tasks.task026.Task26;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args)  {
-        Filter f = new Filter() {
-            @Override
-            public boolean apply(Object o) {
-                if (o instanceof Integer) {
-                    return ((int)o % 2) == 0;
-                }
-                return false;
-            }
-        };
+
 
         List<Integer> list = new ArrayList<>();
         list.add(2);
         list.add(6);
+        list.add(6);
+        list.add(6);
+        list.add(6);
+        list.add(5);
         list.add(5);
         list.add(-1);
         list.add(0);
-        Task25.filter(list, f);
-        System.out.println(list);
+        System.out.println(Task26.deleteDuplicate(list));
 
     }
 }
