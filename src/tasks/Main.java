ef21aee091da2dfb867bef5251c2854fcd10e03d
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-07-09 18:31:09 +0300

    T32

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -1,21 +1,14 @@
 package tasks;
 
 import tasks.task031.Task031;
+import tasks.task032.Task32;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args)  {
-        Thread t = new Thread(new Task031());
-        Thread t2 = new Thread(new Task031());
-        t.setName("Первый");
-        t2.setName("Второй");
-        t.start();
-        t2.start();
-        Thread.yield();
-
-
+        Task32.run();
     }
 }
 
