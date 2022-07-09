8b99173b000864d3e331b215795ccb3a64e55e59
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-07-09 03:11:46 +0300

    t28

--- src/tasks/task028/Task028.java
+++ src/tasks/task028/Task028.java
@@ -1,2 +1,12 @@
-package tasks.task028;public class Task028 {
+package tasks.task028;
+
+public class Task028 {
+    public static void iteratorDV(int line, int column,Object[][] array){
+        for(int i=0;i<line;i++){
+            for(int j=0;j<column;j++){
+                System.out.print(array[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
 }
