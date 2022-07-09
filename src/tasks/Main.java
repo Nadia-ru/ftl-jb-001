b4c51d86c3749288ff6dc200a1708dd510f2e7c5
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-07-09 08:01:16 +0300

    T30

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -3,16 +3,20 @@ package tasks;
 
 import tasks.task028.Task028;
 import tasks.task029.Task29;
+import tasks.task030.MyThread;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args)  {
-        Map<String,Integer> map = new HashMap<>();
-        String[] seasons  = new String[] {"Winter", "Spring", "Spring", "Spring", "Summer","Summer","Summer","Summer","Summer", "Autumn"};
-        map = Task29.arrayToMap(seasons);
-        System.out.println(map);
+
+        MyThread m = new MyThread();
+        m.run();
+
+
+
+
     }
 }
 
